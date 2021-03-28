43a88c931928ec0e947a13a6c4881f0fcae8077c
silverCase <serebrennikov.oleg.p@gmail.com>
2021-03-28 20:16:06 +0300

    cleaned, done 1st real

--- notebooks/A1_DA/main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np, pandas as pd, matplotlib as plt
-
-def main():
-    cpi_data = pd.read_csv(
-        '/Users/oleg/Documents/uni/2-sem/ML/assign/data/A1_Descriptive_Analysis/new_CPI/cpi-2020.csv', sep=";")
-    cpi_data.rename(columns={"CPI score 2020": "cpi"})
-    print()
-if __name__ == '__main__':
-    main()
